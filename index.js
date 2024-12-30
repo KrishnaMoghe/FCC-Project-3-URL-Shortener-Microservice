16c0b56b5e6ff07d4fcda39dfe98ae69389e3160
KrishnaMoghe <krishnamoghe74@gmail.com>
2024-12-30 17:41:16 +0530

    removed URL

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-mongoose.connect("mongodb+srv://Krishnaa:SrE3x41lcII2a3aq@cluster0.nbvzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", 
+mongoose.connect(process.env.MONGO_URL, 
   { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
